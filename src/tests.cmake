f0763df55eded1773934a82221b4496bff256b03
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2021-12-02 11:12:11 +0100

    Test: Add test for intervall class

--- src/tests.cmake
+++ src/tests.cmake
@@ -4,4 +4,9 @@ OPTION(BUILD_INTERVALL_TESTS "Build the tests" ON)
 
 if(${BUILD_INTERVALL_TESTS})
     include(../third_party/googletest.cmake)
+
+    add_executable(UnitTests
+        intervall/intervall.test.cpp)
+    target_link_libraries(UnitTests gtest_main intervalltimer)
+    add_test(UnitTests UnitTests)
 endif()
