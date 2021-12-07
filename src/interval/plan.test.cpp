0bfe7c5ab7a0499e8333cec33091f22f4eda659b
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2021-12-07 16:04:10 +0100

    Test: Refactor the tests for plan

--- src/interval/plan.test.cpp
+++ src/interval/plan.test.cpp
@@ -1,29 +1,63 @@
-#pragma once
+#include <QSignalSpy>
 #include <gtest/gtest.h>
 #include <interval/plan.h>
 
-TEST(Plan, AddInterval) {
-    auto plan = Plan{};
-    auto interval1 = Interval{std::chrono::seconds{1}, "1s"};
-    auto interval2 = Interval{std::chrono::seconds{2}, "2s"};
-    plan.addInterval(interval1);
-    plan.addInterval(interval2);
+struct PlanTesting : ::testing::Test {
+protected:
+    void SetUp() override {
+        plan->appendInterval();
+        plan->appendPlan();
+    }
 
-    EXPECT_EQ(plan.getNumberIntervals(), 2);
-    EXPECT_EQ(plan.getInterval(0), interval1);
-    EXPECT_EQ(plan.getIntervalRef(1), interval2);
+public:
+    Plan* plan = new Plan{};
+};
+
+TEST_F(PlanTesting, appendInterval) {
+    auto intervalItem = plan->getItemAt(0);
+    EXPECT_TRUE(intervalItem.canConvert<Interval>());
+    EXPECT_FALSE(intervalItem.canConvert<Plan*>());
 }
 
-TEST(Plan, RemoveInterval) {
-    auto plan = Plan{};
-    auto interval1 = Interval{std::chrono::seconds{1}, "1s"};
-    auto interval2 = Interval{std::chrono::seconds{2}, "2s"};
-    plan.addInterval(interval1);
-    plan.addInterval(interval2);
+TEST_F(PlanTesting, appendPlan) {
+    auto planItem = plan->getItemAt(1);
+    EXPECT_FALSE(planItem.canConvert<Interval>());
+    EXPECT_TRUE(planItem.canConvert<Plan*>());
+}
 
-    plan.removeInterval(0);
-    EXPECT_EQ(plan.getNumberIntervals(), 1);
-    EXPECT_EQ(plan.getInterval(0), interval2);
+TEST_F(PlanTesting, setInterval) {
+    auto interval = Interval{std::chrono::seconds{2}, "2s"};
+    plan->setItemAt(1, interval);
+
+    EXPECT_EQ(plan->getItemAt(1).value<Interval>(), interval);
+}
+
+TEST_F(PlanTesting, setPlan) {
+    auto planItem = new Plan{};
+    planItem->setNumberRepetitions(42);
+    plan->setItemAt(0, planItem);
+
+    EXPECT_EQ(plan->getItemAt(0).value<Plan*>()->getNumberRepetitions(), planItem->getNumberRepetitions());
+}
+
+TEST_F(PlanTesting, CheckSignalsAppendInterval) {
+    auto preSpy = QSignalSpy(plan, &Plan::preItemAppended);
+    auto postSpy = QSignalSpy(plan, &Plan::postItemAppended);
+
+    plan->appendInterval();
+
+    EXPECT_EQ(preSpy.count(), 1);
+    EXPECT_EQ(postSpy.count(), 1);
+}
+
+TEST_F(PlanTesting, CheckSignalsAppendPlan) {
+    auto preSpy = QSignalSpy(plan, &Plan::preItemAppended);
+    auto postSpy = QSignalSpy(plan, &Plan::postItemAppended);
+
+    plan->appendPlan();
+
+    EXPECT_EQ(preSpy.count(), 1);
+    EXPECT_EQ(postSpy.count(), 1);
 }
 
 TEST(Plan, numberRepetions) {
@@ -32,47 +66,4 @@ TEST(Plan, numberRepetions) {
 
     plan.setNumberRepetitions(42);
     EXPECT_EQ(plan.getNumberRepetitions(), 42);
-}
-
-TEST(Plan, AddPlan) {
-    auto interval1 = Interval{std::chrono::seconds{1}, "1s"};
-    auto interval2 = Interval{std::chrono::seconds{2}, "2s"};
-    auto interval3 = Interval{std::chrono::seconds{3}, "3s"};
-    auto interval4 = Interval{std::chrono::seconds{4}, "4s"};
-    auto plan1 = Plan{};
-    plan1.addInterval(interval1);
-    plan1.addInterval(interval2);
-
-    auto plan2 = Plan{};
-    plan2.addInterval(interval3);
-    plan2.addInterval(interval4);
-
-    auto plan = Plan{};
-    plan.addPlan(plan1);
-    plan.addPlan(plan2);
-
-    EXPECT_EQ(plan.getNumberPlans(), 2);
-    EXPECT_EQ(plan.getPlan(0), plan1);
-    EXPECT_EQ(plan.getPlanRef(1), plan2);
-}
-
-TEST(Plan, RemovePlan) {
-    auto interval1 = Interval{std::chrono::seconds{1}, "1s"};
-    auto interval2 = Interval{std::chrono::seconds{2}, "2s"};
-    auto interval3 = Interval{std::chrono::seconds{3}, "3s"};
-    auto interval4 = Interval{std::chrono::seconds{4}, "4s"};
-    auto plan1 = Plan{};
-    plan1.addInterval(interval1);
-    plan1.addInterval(interval2);
-
-    auto plan2 = Plan{};
-    plan2.addInterval(interval3);
-    plan2.addInterval(interval4);
-
-    auto plan = Plan{};
-    plan.addPlan(plan1);
-    plan.addPlan(plan2);
-    plan.removePlan(0);
-    EXPECT_EQ(plan.getNumberPlans(), 1);
-    EXPECT_EQ(plan.getPlan(0), plan2);
 }
