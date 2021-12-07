1a59d109ff0601e09e83037f1fa05d8d551484a2
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2021-12-07 16:02:58 +0100

    Refactor: use QVariant instead of independend interval and plan vectors
    * make Plan to a QObject
    * remove all unneeded functions

--- src/interval/plan.cpp
+++ src/interval/plan.cpp
@@ -1,18 +1,36 @@
 #include "plan.h"
+#include <exception>
+Plan::Plan(QObject* parent)
+    : QObject(parent) {}
 
-Plan::Plan() {}
+void Plan::setItemAt(size_t const& index, Plan* plan) {
+    if (index >= items.size()) {
+        throw std::range_error{"index out of range"};
+    }
+    items[index] = QVariant::fromValue<Plan*>(plan);
+}
 
-void Plan::addPlan(Plan const& plan) { plans.push_back(plan); }
-Plan const& Plan::getPlan(size_t const& index) const { return plans.at(index); }
-Plan& Plan::getPlanRef(size_t const& index) { return plans.at(index); }
-void Plan::removePlan(size_t const& index) { plans.erase(plans.begin() + index); }
-size_t Plan::getNumberPlans() const { return plans.size(); }
+void Plan::setItemAt(size_t const& index, Interval const& interval) {
+    if (index >= items.size()) {
+        throw std::range_error{"index out of range"};
+    }
+    items[index] = QVariant::fromValue(interval);
+}
 
-void Plan::addInterval(Interval const& interval) { intervals.push_back(interval); }
-Interval const& Plan::getInterval(size_t const& index) const { return intervals.at(index); }
-Interval& Plan::getIntervalRef(size_t const& index) { return intervals.at(index); }
-void Plan::removeInterval(size_t const& index) { intervals.erase(intervals.begin() + index); }
-size_t Plan::getNumberIntervals() const { return intervals.size(); }
+void Plan::appendInterval() {
+    emit preItemAppended();
+    items.push_back(QVariant::fromValue(Interval{}));
+    emit postItemAppended();
+}
+void Plan::appendPlan() {
+    emit preItemAppended();
+    items.push_back(QVariant::fromValue<Plan*>(new Plan{}));
+    emit postItemAppended();
+}
+
+QVariant Plan::getItemAt(const size_t& index) { return items.at(index); }
+
+QVariantList Plan::getItems() { return items; }
 
 uint32_t Plan::getNumberRepetitions() const { return numberRepetitions; }
 void Plan::setNumberRepetitions(uint32_t const& repetitions) { numberRepetitions = repetitions; }
