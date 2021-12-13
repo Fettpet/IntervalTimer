d8bbb15a0f2e755029e78bda40afc6c7168c4b79
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2021-12-13 11:23:50 +0100

    Test: Check the correctness of Interval to/from JSON

--- src/Interval/Interval.test.cpp
+++ src/Interval/Interval.test.cpp
@@ -1,4 +1,6 @@
 #include <Interval.h>
+#include <IntervalFromJson.h>
+#include <IntervallToJson.h>
 #include <gtest/gtest.h>
 
 TEST(Interval, duration) {
@@ -13,4 +15,20 @@ TEST(Interval, description) {
     interval.setDescripton("Hello World");
 
     EXPECT_EQ(interval.getDescripton(), std::string{"Hello World"});
+}
+
+TEST(Interval, toJson) {
+    auto interval = Interval{std::chrono::seconds{10}, "Hello World"};
+    auto json = IntervallToJson::transform(interval);
+    EXPECT_EQ(json["description"], "Hello World");
+    EXPECT_EQ(json["durationMilliseconds"], 10000);
+}
+
+TEST(Interval, fromJson) {
+    auto interval = Interval{std::chrono::seconds{10}, "Hello World"};
+    auto json = IntervallToJson::transform(interval);
+    auto transformedInterval = IntervalFromJson::transform(json);
+
+    EXPECT_EQ(interval.getDescripton(), transformedInterval.getDescripton());
+    EXPECT_EQ(interval.getDuration<std::chrono::seconds>(), transformedInterval.getDuration<std::chrono::seconds>());
 }
