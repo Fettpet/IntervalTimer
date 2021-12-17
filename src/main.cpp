e76039ede4f30807cefe3ad9ca07ebd25e255d96
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2021-12-17 14:42:08 +0100

    Deletable: Add a basic example to main. This comment can be removed

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,35 @@ int main(int argc, char* argv[]) {
     }
 
     QQmlApplicationEngine engine;
-    Plan plan{};
-    engine.rootContext()->setContextProperty(QStringLiteral("rootPlan"), &plan);
+    auto nestedPlan = std::make_shared<Plan>();
+    nestedPlan->setName("Inner");
+    nestedPlan->setNumberRepetitions(12);
+    nestedPlan->appendInterval();
+    nestedPlan->appendInterval();
+    nestedPlan->setItemAt(0, Interval{std::chrono::seconds{3}, "third"});
+    nestedPlan->setItemAt(1, Interval{std::chrono::seconds{4}, "fourth"});
+
+    auto plan = std::make_shared<Plan>();
+    plan->setName("Outer");
+    plan->setNumberRepetitions(10);
+    plan->appendInterval();
+    plan->appendInterval();
+    plan->appendPlan();
+    auto outerFirst = Interval{std::chrono::seconds{1}, "first"};
+    auto outerSecond = Interval{std::chrono::seconds{2}, "second"};
+    plan->setItemAt(0, outerFirst);
+    plan->setItemAt(1, outerSecond);
+    plan->setItemAt(2, nestedPlan);
+
+    auto planToModel = std::make_shared<Plan>();
+    planToModel->appendPlan();
+    planToModel->setName("Wurzel");
+    planToModel->setItemAt(0, plan);
+
+    auto model = PlanModel();
+    model.setPlan(plan);
+
+    engine.rootContext()->setContextProperty(QStringLiteral("rootPlanModel"), &model);
 
     const QUrl url(u"qrc:/IntervalApplication/main.qml"_qs);
     QObject::connect(
