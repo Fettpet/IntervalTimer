f10bfc7ac4aa79e6e095b31b1d675c3d232c4a97
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2022-02-01 10:42:54 +0100

    Test: Add Tests for the rowCount function for PlanStorageModel
    * test count after initialize
    * test count after insert/remove plan
    * test the database after insert/remove plan

--- src/tests.cmake
+++ src/tests.cmake
@@ -14,6 +14,7 @@ if(${BUILD_INTERVAL_TESTS})
         Intervaltimer/intervaltimer/PlanIterator.test.cpp
         Intervaltimer/intervaltimer/PlanModel.test.cpp
         Intervaltimer/intervaltimer/PlanRunner.test.cpp
+        Intervaltimer/intervaltimer/PlanStorageModel.test.cpp
         Intervaltimer/intervaltimer/Timer.test.cpp
         Intervaltimer/intervaltimer/main.test.cpp
     )
