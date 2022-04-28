d1589773855bed07b867e8371546a02daea4c674
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2022-04-28 13:06:02 +0200

    Chore: Add nodiscard

--- src/Intervaltimer/intervaltimer/PlanModel.h
+++ src/Intervaltimer/intervaltimer/PlanModel.h
@@ -70,9 +70,9 @@ protected:
     [[nodiscard]] QVariant getDataForPlan(const QModelIndex& index, int role) const;
     [[nodiscard]] QVariant getDataForInterval(const QModelIndex& index, int role) const;
 
-    bool setDataForPlan(const QModelIndex& index, const QVariant& value, int role);
+    [[nodiscard]] bool setDataForPlan(const QModelIndex& index, const QVariant& value, int role);
     [[nodiscard]] QVariant getDataForSubPlan(const QModelIndex& index, int role) const;
-    bool setDataForInterval(const QModelIndex& index, const QVariant& value, int role);
+    [[nodiscard]] bool setDataForInterval(const QModelIndex& index, const QVariant& value, int role);
 
     [[nodiscard]] bool isDataSetable(const QModelIndex& index, const QVariant& value, int role) const;
 
