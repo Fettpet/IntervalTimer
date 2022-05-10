7233620fe0e7446bd62479ff567316ccece3db00
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2022-05-10 09:43:40 +0200

    Refactor: Make the functions virtual

--- src/Intervaltimer/intervaltimer/PlanStorageBuffer.h
+++ src/Intervaltimer/intervaltimer/PlanStorageBuffer.h
@@ -6,13 +6,13 @@ class PlanStorageBuffer {
 public:
     PlanStorageBuffer() = default;
 
-    void insert(QString const& name, std::shared_ptr<Plan> const& plan);
-    [[nodiscard]] bool contains(QString const& name);
-    void remove(QString const& name);
-    [[nodiscard]] QString getKey(size_t const& index);
-    [[nodiscard]] QList<QString> keys() const;
-    [[nodiscard]] std::shared_ptr<Plan> getValue(QString const& key);
-    [[nodiscard]] std::shared_ptr<Plan> getValue(size_t const& index);
+    virtual void insert(QString const& name, std::shared_ptr<Plan> const& plan);
+    [[nodiscard]] virtual bool contains(QString const& name);
+    void virtual remove(QString const& name);
+    [[nodiscard]] virtual QString getKey(size_t const& index);
+    [[nodiscard]] virtual QList<QString> keys() const;
+    [[nodiscard]] virtual std::shared_ptr<Plan> getValue(QString const& key);
+    [[nodiscard]] virtual std::shared_ptr<Plan> getValue(size_t const& index);
 
 private:
     QMap<QString, std::shared_ptr<Plan>> data;
